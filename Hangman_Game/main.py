015238d85543d4a8a88c3b482d4673da716af857
Tharun Reddy Rachamalla <55098125+InertExpert2911@users.noreply.github.com>
2022-01-16 21:52:05 +0530

    Add files via upload

---
+++ Hangman_Game/main.py
@@ -0,0 +1,55 @@
+import random
+from hangman_art import stages, logo
+from hangman_words import word_list
+
+# To clear the screen after each guess
+from replit import clear
+
+print(logo)
+game_is_finished = False
+lives = len(stages) - 1
+
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+#Testing code
+# print(f'Pssst, the solution is {chosen_word}.')
+
+# Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not game_is_finished:
+    guess = input("Guess a letter: ").lower()
+
+    # Making use of the clear() to clear screen after each guess.
+    clear()
+
+    # If the user has entered a letter they've already guessed, print the letter.
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
+    # Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+    print(f"{' '.join(display)}")
+
+    # Check if user is wrong.
+    if guess not in chosen_word:
+        # If the letter is not in the chosen_word, print out the letter and let user know.
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        lives -= 1
+        if lives == 0:
+            game_is_finished = True
+            print("You lose.")
+    
+    #Check if user has got all letters.
+    if not "_" in display:
+        game_is_finished = True
+        print("You win.")
+
+    print(stages[lives])
