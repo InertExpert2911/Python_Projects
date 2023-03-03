4539d59a8392cdc898391631f6eedfcf530c562e
Tharun Reddy Rachamalla <reachme.tharun@gmail.com>
2023-03-03 22:54:35 +0530

    2023-03-03

--- Projects/Beginner/2 - Tip_Calculator.py
+++ Projects/Beginner/2 - Tip_Calculator.py
@@ -7,13 +7,16 @@ tip_percentage = int(input("How much tip would you like to give? 10, 12, or 15 p
 total_people = int(input("How many people to split the bill? "))
 
 # (1 + tip_percentage / 100) => We are doing this to add the tip amount directly to the bill
-# Ex: Each person should pay (150.00 / 5) * 1.12 = 33.6
+# Eg: Each person should pay (150.00 / 5) * 1.12 = 33.6
 bill_amount_to_pay = (bill_amount / total_people) * (1 + tip_percentage / 100)
 
+# ----------------------------------------------------------------------------------------------------------
+
 # SOLUTION-1: Rounding-off to 2 decimal places
-# EX: (number, number_of_digits)
+# Eg: (number, number_of_digits)
 # print(f"Each person should pay: ${round(bill_amount_to_pay, 2)}")
 
+# ----------------------------------------------------------------------------------------------------------
 
 # SOLUTION-2: Using format(num, ".2f") to format the amount to 2 decimal digits
 rounded_off_amount_to_pay = format(bill_amount_to_pay, ".2f")
