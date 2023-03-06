d368e03c3a609c6a4b226930c97ac5fda87121d5
Tharun Reddy Rachamalla <reachme.tharun@gmail.com>
2023-03-06 19:56:00 +0530

    06-03-2023

--- Projects/Beginner/6 - Reboorg_Maze.py
+++ Projects/Beginner/6 - Reboorg_Maze.py
@@ -1,3 +1,22 @@
 # Access the below link to run the code
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
 
+# Function to turn right
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+# Loop till you reach the goal
+while not at_goal():
+    # If right is clear, then turn right and move
+    if right_is_clear():
+        turn_right()
+        move()
+    # Else if front is clear, move forward
+    elif front_is_clear():
+        move()
+    # If none of the above cases satisfy, then turn left
+    else:
+        turn_left()
