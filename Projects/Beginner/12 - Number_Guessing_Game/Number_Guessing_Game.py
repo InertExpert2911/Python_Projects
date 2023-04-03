96de3970a2bd26d5a0ab82350ff3707fb8ab6a3d
Tharun Reddy Rachamalla <reachme.tharun@gmail.com>
2023-04-03 22:52:45 +0530

    Update Number_Guessing_Game.py

--- Projects/Beginner/12 - Number_Guessing_Game/Number_Guessing_Game.py
+++ Projects/Beginner/12 - Number_Guessing_Game/Number_Guessing_Game.py
@@ -5,16 +5,27 @@ from number_guessing_game_logo import logo
 
 def random_number_generator():
     """Returns a random integer number between 1 and 100, inclusive of both numbers."""
+    
     return random.randint(1, 100)
 
 
+def compare_guess():
+    """Handles user guess and returns "Too High" or "Too Low" or if the user found the number based on users guess"""
+    
+    user_guess = int(input("Make a guess: "))
+    
+    if user_guess == random_number:
+        return f"You got the number! The number was {random_number}"
+    elif user_guess > random_number:
+        return "Too High."
+    elif user_guess < random_number:
+        return "Too Low."
 
 
 
 
 
-
-
+user_lives = 0
 
 # print(logo)
 print("Welcome to the number guessing game!")
@@ -24,9 +35,7 @@ random_number = random_number_generator()
 
 print(f"Psst, the number I guessed is {random_number}")
 
-
 game_difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
-
 
 if game_difficulty == 'easy':
     user_lives = 10
@@ -34,29 +43,15 @@ elif game_difficulty == 'hard':
     user_lives = 5
 else:
     print("Invalid input")
-    
-
-
-
-def compare_guess():
-    """Handles user guess and returns "Too High" or "Too Low" or if the user found the number guessed based on users guess"""
-
-    user_guess = int(input("Make a guess: "))
-
-    if user_guess == random_number:
-        return f"You got the number! The number was {random_number}"
-    elif user_guess > random_number:
-        return "Too High."
-    else:
-        return "Too Low."
+    \
 
 while user_lives != 0:
     
-    return_compare_guess = compare_guess()
-    print(return_compare_guess)
-    
     print(f"You have {user_lives} attempts remaining to guess the number.")
     
+    print(compare_guess())
+    
     user_lives -= 1
+
 
 
