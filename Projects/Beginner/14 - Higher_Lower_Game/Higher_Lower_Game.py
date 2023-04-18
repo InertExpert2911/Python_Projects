f8d7aec3625404c54e6dd0b13e3cae24c0ceea16
Tharun Reddy Rachamalla <reachme.tharun@gmail.com>
2023-04-18 21:49:28 +0530

    Update Higher_Lower_Game.py

--- Projects/Beginner/14 - Higher_Lower_Game/Higher_Lower_Game.py
+++ Projects/Beginner/14 - Higher_Lower_Game/Higher_Lower_Game.py
@@ -3,66 +3,42 @@ from higher_lower_game_logo import logo, vs
 
 import random
 
-# print(logo)
-
-# TODO-1: GET A RANDOM item from the dictionary
-
 
 def get_random_account():
-    """Generates a random number to fetch data from the data list and returns the user account details."""
+    """Generates a random number and gets the index from the data list and gets all the values in the dictionary and returns all those values."""
 
     random_list_index = random.randint(0, len(data) - 1)
     user_account = data[random_list_index]
-    return user_account
-
-
-def extract_user_details(user_account):
-    """Extract user details like name, description, country and follower count from a dictionary and returns"""
-
+    
     user_name = user_account['name']
-    description = user_account['description']
+    user_description = user_account['description']
     user_country = user_account['country']
     follower_count = user_account['follower_count']
-
     
-    # print(f"Compare A : {user_name}, a {description}, from {user_country}")
+    return user_name, user_description, user_country, follower_count
 
 
-account_1 = get_random_account()
-account_details_1 = extract_user_details(account_1)
+account = get_random_account()
+print(account)
 
-print(account_details_1.user_name)
+print(f"Compare A: {account[0]}, a {account[1]}, from {account[2]}")
 
+user_score = 0
 
-# account_2 = get_random_account()
+user_input = input("Who has more followers? Type 'A' or 'B': ").lower()
 
-# user_name_2 = account_2['name']
-# description_2 = account_2['description']
-# user_country_2 = account_2['country']
-# follower_count_2 = account_2['follower_count']
+if user_input == 'a':
+    if follower_count_1 > follower_count_2:
+        user_score += 1
+        print(f"You're right! Current score: {user_score}.")
 
-# print(f"Against B : {user_name_2}, a {description_2}, from {user_country_2}")
-
-
-# print(f"A - {follower_count_1}")
-# print(f"B - {follower_count_2}")
-
-# user_score = 0
-
-# user_input = input("Who has more followers? Type 'A' or 'B': ").lower()
-
-# if user_input == 'a':
-#     if follower_count_1 > follower_count_2:
-#         user_score += 1
-#         print(f"You're right! Current score: {user_score}.")
-
-#     else:
-#         # TODO: print logo, clear screen, end game
-#         print(f"Sorry, you got it wrong. Final score {user_score}.")
-# elif user_input == 'b':
-#     if follower_count_2 > follower_count_1:
-#         user_score += 1
-#         print(f"You're right! Current score: {user_score}.")
-#     else:
-#         # TODO: print logo, clear screen, end game
-#         print(f"Sorry, you got it wrong. Final score {user_score}.")
+    else:
+        # TODO: print logo, clear screen, end game
+        print(f"Sorry, you got it wrong. Final score {user_score}.")
+elif user_input == 'b':
+    if follower_count_2 > follower_count_1:
+        user_score += 1
+        print(f"You're right! Current score: {user_score}.")
+    else:
+        # TODO: print logo, clear screen, end game
+        print(f"Sorry, you got it wrong. Final score {user_score}.")
